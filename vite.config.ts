b93784353cb0a65b51aaafb2b0ce1c766b6b9b87
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2023-07-22 12:59:11 +0200

    set up solid labels

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
 import devtools from "solid-devtools/vite";
+import solidLabels from "vite-plugin-solid-labels";
 
 export default defineConfig({
   plugins: [
+    solidLabels({
+      filter: {
+        include: "src/**/*.{ts,tsx}",
+        exclude: "node_modules/**/*.{ts,js,tsx,jsx}",
+      },
+    }),
     devtools({
       autoname: true,
       locator: {

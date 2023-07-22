4dc42dedba3fca9dea700cedfb7097cad9a345ed
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2023-07-22 20:13:48 +0200

    WIP

--- src/components/HexGrid/HexGrid.tsx
+++ src/components/HexGrid/HexGrid.tsx
@@ -1,26 +1,25 @@
 import { Index, createSignal, onMount } from "solid-js";
 import type { THex } from "../../types/map";
-import { Hex } from "../Hex/Hex";
 import styles from "./HexGrid.module.css";
+import { Hex } from "../Hex/Hex";
 import { createStore, produce } from "solid-js/store";
 import { getInitialMap, getStartUnitPositions } from "./helpers/map";
-// import { TUnitInstance } from "../../types/Unit";
 import { getInitialUnits, getMovementArea } from "./helpers/unit";
 import { TTribes } from "../../constants/tribe";
 import { getMapHex, getIsEven } from "../../utils/map";
 import { HexState } from "../../constants/hex";
+import { TUnitInstance } from "../../types/unit";
 
-export type TSelectedHex =
-  | (THex & {
-      state: string;
-    })
-  | null;
+export type TSelectedHex = (THex & { state: string }) | null;
 
 export const HexGrid = (props: { tribes: TTribes[] }) => {
   const [map, setMap] = createStore(getInitialMap(props.tribes.length));
   const [units] = createStore(getInitialUnits(props.tribes));
 
   const [selectedHex, setSelectedHex] = createSignal<TSelectedHex>(null);
+  const [selectedUnit, setSelectedUnit] = createSignal<TUnitInstance | null>(
+    null,
+  );
   const [highlighted, setHighlighted] = createSignal<
     ReturnType<typeof getMovementArea>
   >({});
@@ -28,10 +27,10 @@ export const HexGrid = (props: { tribes: TTribes[] }) => {
   onMount(() => {
     const startPositions = getStartUnitPositions(props.tribes, units);
 
-    startPositions.forEach(({ unitId, row, col }) => {
+    startPositions.forEach(({ col, row, unitId }) => {
       setMap(
         produce((map) => {
-          const hex = getMapHex(map, { row, col });
+          const hex = getMapHex(map, { col, row });
           hex.unitId = unitId;
         }),
       );
@@ -43,7 +42,7 @@ export const HexGrid = (props: { tribes: TTribes[] }) => {
     const isUnitSelected = hex?.state === HexState.Unit;
 
     if (!isUnitSelected || !hex?.unitId) {
-      setHighlighted({ rows: [], cols: [] });
+      setHighlighted({ cols: [], rows: [] });
       return;
     }
 
@@ -83,8 +82,8 @@ export const HexGrid = (props: { tribes: TTribes[] }) => {
           return (
             <div
               classList={{
-                [styles.row]: true,
                 [styles.isEven]: getIsEven(rowIndex),
+                [styles.row]: true,
               }}
             >
               <Index each={row()}>

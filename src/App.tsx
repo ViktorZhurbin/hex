3a4c12349c0d01705598f6c37f66eb904fd2d4e7
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2023-08-05 21:54:09 +0200

    add drei

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { OrbitControls } from "@react-three/drei/core/OrbitControls";
 import { Canvas } from "@react-three/fiber";
 
 import "./App.css";
@@ -5,11 +6,12 @@ import { Box } from "./components/Box/Box";
 
 function App() {
   return (
-    <Canvas>
+    <Canvas camera={{ fov: 50, position: [0, 0, 3] }}>
       <ambientLight />
       <pointLight position={[4, 1, 10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
+      <OrbitControls dampingFactor={0.3} makeDefault />
     </Canvas>
   );
 }

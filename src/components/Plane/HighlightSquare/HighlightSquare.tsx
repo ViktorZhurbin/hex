d359f24977a8d989fa81874e89d347b518175c23
Viktor Zhurbin <vzhurbin@gmail.com>
2023-08-22 03:34:19 +0200

    use string color

--- src/components/Plane/HighlightSquare/HighlightSquare.tsx
+++ src/components/Plane/HighlightSquare/HighlightSquare.tsx
@@ -1,8 +1,5 @@
 import { Vector3 } from "@react-three/fiber";
 import { DoubleSide } from "three/src/constants.js";
-import { Color } from "three/src/math/Color.js";
-
-const color = new Color(0xfffff);
 
 export const HighlightSquare = ({ position }: { position: Vector3 | null }) => {
   if (!position) {
@@ -12,7 +9,7 @@ export const HighlightSquare = ({ position }: { position: Vector3 | null }) => {
   return (
     <mesh position={position} rotation-x={-Math.PI / 2}>
       <planeGeometry args={[1, 1]} />
-      <meshStandardMaterial color={color} side={DoubleSide} />
+      <meshStandardMaterial color={"white"} side={DoubleSide} />
     </mesh>
   );
 };

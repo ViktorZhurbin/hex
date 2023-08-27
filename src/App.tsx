3569e7858447a6d8b26900a1eaafb110ed623b5b
Viktor Zhurbin <vzhurbin@gmail.com>
2023-08-27 15:59:08 +0200

    move init out of component

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,25 @@ import { Canvas } from "@react-three/fiber";
 import { Map } from "./components/Map/Map";
 import { initMap } from "./utils/map/initMap";
 
-function App() {
-  enableReactUse();
-  initMap();
+enableReactUse();
+initMap();
 
-  return (
-    <Canvas camera={{ fov: 40, position: [10, 10, 0] }}>
-      <ambientLight />
+const App = () => (
+  <Canvas camera={{ fov: 40, position: [10, 10, 0] }}>
+    <ambientLight />
 
-      <Map />
+    <Map />
 
-      {/* map-like camera controls */}
-      <MapControls
-        enableRotate={false}
-        makeDefault
-        maxDistance={50}
-        minDistance={5}
-        minZoom={10}
-        panSpeed={2}
-      />
-    </Canvas>
-  );
-}
+    {/* map-like camera controls */}
+    <MapControls
+      enableRotate={false}
+      makeDefault
+      maxDistance={50}
+      minDistance={5}
+      minZoom={10}
+      panSpeed={2}
+    />
+  </Canvas>
+);
 
 export default App;

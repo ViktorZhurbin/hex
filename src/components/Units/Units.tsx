bb44eab4f6363478600218f44e6ec2cba81c4283
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-12-16 22:59:40 +0100

    Load first model

--- src/components/Units/Units.tsx
+++ src/components/Units/Units.tsx
@@ -3,8 +3,10 @@ import { For } from "@legendapp/state/react";
 import { unitIds$ } from "../../state/selectors/units";
 import { Unit } from "../Unit/Unit";
 
-export const Units = () => (
-	<For each={unitIds$} optimized>
-		{(unitId$) => <Unit unitId={unitId$.get()} />}
-	</For>
-);
+export const Units = () => {
+	return (
+		<For each={unitIds$} optimized>
+			{(unitId$) => <Unit unitId={unitId$.get()} />}
+		</For>
+	);
+};

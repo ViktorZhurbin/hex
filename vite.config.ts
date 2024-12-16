3a8bc36961c9bca0650d85203e74264dfdbdb2a6
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-12-16 22:00:11 +0100

    upgrade vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import react from "@vitejs/plugin-react";
-import { defineConfig, splitVendorChunkPlugin } from "vite";
+import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
 		react(),
-		splitVendorChunkPlugin(),
 		checker({
 			typescript: true,
 			overlay: false,

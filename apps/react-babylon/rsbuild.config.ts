5107c9378528b9c67879bdb945a3bc4106d0ec91
Viktor Zhurbin <viktor.zhurbin@pumble.com>
2024-12-18 22:42:21 +0100

    add react-babylon app

---
+++ apps/react-babylon/rsbuild.config.ts
@@ -0,0 +1,32 @@
+import path from "node:path";
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginTypeCheck } from "@rsbuild/plugin-type-check";
+
+export default defineConfig({
+	plugins: [pluginReact(), pluginTypeCheck()],
+
+	server: {
+		port: 5173,
+	},
+
+	html: {
+		template: "./static/index.html",
+	},
+	source: {
+		entry: {
+			index: "./src/main.tsx",
+		},
+
+		assetsInclude: /\.(glb)$/,
+	},
+
+	output: {
+		copy: [
+			{
+				from: path.resolve("../../libs/assets/src"),
+				to: import.meta.env.PUBLIC_STATIC_ASSETS_PATH,
+			},
+		],
+	},
+});
